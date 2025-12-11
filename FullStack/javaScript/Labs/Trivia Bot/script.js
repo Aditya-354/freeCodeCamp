33f98d2795e46463c51c1a61d5ff236f26be5fad
Aditya-354 <adigithub354@gmail.com>
2025-12-11 20:33:16 +0530

    first commit

---
+++ FullStack/javaScript/Labs/Trivia Bot/script.js
@@ -0,0 +1,18 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "triviaBot";
+let botLocation = "your computer";
+let favoriteLanguage = "JavaScript";
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = favoriteLanguage + " is used for web development.";
+console.log(codingFact);
+
+codingFact = "Web developers use " + favoriteLanguage + " to add dynamic features to their web pages.";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " is a programming language unlike HTML and CSS that are markup languages.";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
---
+++ README.md
@@ -0,0 +1,9 @@
+### freeCodeCamp Courses
+
+I started my freecodeCamp journey on 10-12-2025.
+
+---
+
+## JavaScript:
+
+- [WorkShops]()
